a579cec20d819ca03406528aefc858c4ab2a8395
leofinder <imrhyde@yandex.ru>
2022-07-28 22:38:54 +0500

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,5 +7,7 @@ public class StartUI {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println("Item created: " + item.getCreated().format(formatter));
+        Item item2 = new Item(2, "Item 2");
+        System.out.println(item2);
     }
 }
