002e63e78d7f892ec1d3c06e5c604bdd48d1e419
leofinder <imrhyde@yandex.ru>
2022-08-01 22:10:23 +0500

    3. Интерфейс Input [#181092 #327371]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,19 +1,22 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport, Vehicle {
+    private final static double OIL_PRICE = 0.75;
+    private boolean isMoved;
+
     @Override
     public void drive() {
-
+        isMoved = true;
     }
 
     @Override
     public void passengers(int count) {
-
+        System.out.println("Количество пассажиров в автобусе: " + count);
     }
 
     @Override
     public double tankup(int count) {
-        return 0;
+        return OIL_PRICE * count;
     }
 
     @Override
