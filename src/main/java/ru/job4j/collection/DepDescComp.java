a0e1b5c3b62336b5983e5fd518dc3deb99093033
leofinder <imrhyde@yandex.ru>
2022-09-11 16:55:17 +0500

    2. Отсортировать департаменты [#34352 #342025]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,14 +8,9 @@ import java.util.List;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String d1, String d2) {
-        List<String> dep1 = Departments.fillGaps(Collections.singletonList(d1));
-        List<String> dep2 = Departments.fillGaps(Collections.singletonList(d2));
-        for (int i = 0; i < Math.min(dep1.size(), dep2.size()); i++) {
-            int rsl = i == 0 ? dep2.get(i).compareTo(dep1.get(i)) : dep1.get(i).compareTo(dep2.get(i));
-            if (rsl != 0) {
-                return rsl;
-            }
-        }
-        return Integer.compare(dep1.size(), dep2.size());
+        String[] dep1 = d1.split("/");
+        String[] dep2 = d2.split("/");
+        int rsl = dep2[0].compareTo(dep1[0]);
+        return rsl == 0 ? d1.compareTo(d2) : rsl;
     }
 }
